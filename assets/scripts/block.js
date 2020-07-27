e5b605ae5781d003fee6a59e02ba6be2f5dd41e2
joanwu <joanwu0615@gmail.com>
2020-07-27 23:09:13 +0800

    bugfix

--- assets/scripts/block.js
+++ assets/scripts/block.js
@@ -1,29 +1,21 @@
-import colors from 'colors';
+import colors from "colors"
 
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        numberLabel: cc.Label
-    },
+  properties: {
+    numberLabel: cc.Label,
+  },
 
-    // LIFE-CYCLE CALLBACKS:
+  start() {},
 
-    // onLoad () {},
-
-    start () {
-
-    },
-
-    setNumber (number) {
-        if (number == 0) {
-            this.numberLabel.node.active = false;
-        }
-        this.numberLabel.string = number;
-        if (number in colors) {
-            this.node.color = colors[number];
-        }
+  setNumber(number) {
+    if (number == 0) {
+      this.numberLabel.node.active = false
     }
-
-    // update (dt) {},
-});
+    this.numberLabel.string = number
+    if (number in colors) {
+      this.node.color = colors[number]
+    }
+  },
+})
